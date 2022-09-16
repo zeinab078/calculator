bf91ba9e69549b7ff1eef0a76cf9e2c79f394e82
zeinab Ariyan Pour <zeinabariyanpour1999@gmail.com>
2022-09-16 04:42:59 -0700

    write clear last function to delete the last number

--- calcu.js
+++ calcu.js
@@ -19,8 +19,17 @@ function showDisplay(event){
     displayBox.innerHTML=0;
   }
 
+  function clearLast(){
+    let text=displayBox.innerText;
+    if(text.length===1){
+        displayBox.innerText= 0;
+    }else{
+        displayBox.innerText=text.substring(0,text.length-1)
+    }
+}
+  
 
 
-
+document.querySelector(".clear-last").addEventListener("click",clearLast)
 document.querySelector(".all-clear").addEventListener("click",clearAll)
 document.querySelector(".calculate").addEventListener("click",calculation)
